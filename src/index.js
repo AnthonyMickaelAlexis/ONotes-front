574e6624e3347af2cac4a46115c7ed2a2c973a0e
XvR <xboiry@gmail.com>
2023-10-09 09:34:28 +0200

    Feature done

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './reducers/store';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/reset.scss';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -7,6 +7,9 @@ import startAnimation from '../../utils/fallingTags';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import CarouselContainer from '../../utils/carouselContainer';
+import ArrowRight from '../../assets/images/arrow-right.svg';
+import NavigationButtonComponent from '../../components/NavigationButtonComponent';
+import { ModifyHalo } from '../../utils/haloModifier';
 
 
 function Homepage() {
@@ -19,20 +22,39 @@ function Homepage() {
   ]
 
   const canvas = useRef();
-  const alreadyStarted = useRef(false);
+  const halo1 = useRef();
+  const halo2 = useRef();
+  const halo3 = useRef();
+  let halo1Obj, halo2Obj, halo3Obj;
   useEffect(() => {
-    if (alreadyStarted.current === false) {
-      startAnimation(canvas.current);
+    startAnimation(canvas.current);
+    halo1Obj = new ModifyHalo(halo1.current);
+    halo1Obj.start();
+    halo2Obj = new ModifyHalo(halo2.current);
+    setTimeout(() => {
+      halo2Obj.start();
+    }, 1000);
+    halo3Obj = new ModifyHalo(halo3.current);
+    setTimeout(() => {
+      halo3Obj.start();
+    }, 2000);
+
+    return () => {
+      halo1Obj.stop();
+      halo2Obj.stop();
+      halo3Obj.stop();
     }
-    alreadyStarted.current = true;
   }, [])
 
   return (
     <div className="homepage">
+      <div ref={halo1} className='homepage-halo1'></div>
+      <div ref={halo2} className='homepage-halo2'></div>
+      <div ref={halo3} className='homepage-halo3'></div>
       {fallingTags.map(tagElement =>
         <Tag key={`tag${tagElement.key}`} icon={tagElement.icon} text={tagElement.text} textColor={tagElement.textColor} bgColor={tagElement.bgColor} />
       )}
-      <div ref={canvas} className='home-falling-tags'></div>
+      <div ref={canvas} className='homepage-falling-tags-canvas'></div>
       <Link to="/authentication">
         Auth
       </Link>
@@ -40,17 +62,32 @@ function Homepage() {
       <div>logo</div>
       <div> login button</div>
       {/* HEADER END */}
-      <div>title</div>
-        <div>subtitle</div>
-        <div>button discover</div>
-        <div>components matterjs qui tombent</div>
-        <div> titre au-dessus d&#39;article </div>
-        {/* carousel */}
-    
+      <section className='homepage-title-and-cta'>
+        <h1>Une application pour tout savoir sur tout !</h1>
+        <p>O&apos;Notes est une plateforme dynamique conçue pour les étudiants passionnés par l&apos;apprentissage collaboratif et la maîtrise des technologies.</p>
+        <NavigationButtonComponent text='DECOUVRIR' icon={ArrowRight} textColor='white' bgColor='black' link='/authentication' />
+      </section>
+      <section className='homepage-part-2'>
+        <div className='homepage-part-2-title'>
+          <h2>Un espace où les octets se métamorphosent en neurones !</h2>
+        </div>
         <CarouselContainer />
-
-{/* carousel end*/}
-        <div> les chiffres qui font parler</div>
+        <div className='homepage-part-2-stats'>
+          <div>Des chiffres qui font parler...</div>
+          <div className='homepage-part-2-stats-single'>
+            <p>99%</p>
+            <p>de sujets résolus</p>
+          </div>
+          <div className='homepage-part-2-stats-single'>
+            <p>12K</p>
+            <p>utilisateurs</p>
+          </div>
+          <div className='homepage-part-2-stats-single'>
+            <p>+100</p>
+            <p>tags et catégories</p>
+          </div>
+        </div>
+      </section>
         
         <div> box autour de image communauté
         <div> component image</div>
