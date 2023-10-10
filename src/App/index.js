e946d787b9c91ecff7363067dc13ed787a852118
Mickaël <yellownanaki@gmail.com>
2023-10-10 15:25:38 +0200

    finishing to show footer only on mainpage and show header on each page except authentication page

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Auth from "../views/Auth";
 import Homepage from '../views/Homepage';
 import Layout from '../utils/layout';
 
 function App() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === '/authentication';
+  const isHomePage = location.pathname === '/';
 
   return (
     <div className="App">
-      <Layout>
+      <Layout showHeader={!isAuthPage} showFooter={isHomePage}>
         <Routes>
           <Route path="/" element={ <Homepage /> } />
           <Route path="/authentication" element={ <Auth /> } />
