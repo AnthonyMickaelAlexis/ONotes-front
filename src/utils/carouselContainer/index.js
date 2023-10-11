a59a8af2416c08ecefb12b47ed3a05382418fa96
Mickaël <yellownanaki@gmail.com>
2023-10-11 13:08:45 +0200

    need to fix display of number of slides for mobile or desktop

--- src/utils/carouselContainer/index.js
+++ src/utils/carouselContainer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay, FreeMode } from 'swiper/modules';
 import 'swiper/css';
@@ -10,33 +10,52 @@ import { useGetArticlesQuery } from '../../data/articles';
 
 function CarouselContainer() {
     const { data: articles, loading: articlesLoading, error: articlesError } = useGetArticlesQuery();
+    const [slidesPerView, setSlidesPerView] = useState(4);
+    const [showPagination, setShowPagination] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth <= 600) {
+            setSlidesPerView(2);
+            setShowPagination(false);
+          } else {
+            setSlidesPerView(4);
+            setShowPagination(true);
+          }
+        };
+    
+        window.addEventListener('resize', handleResize);
+        handleResize();
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
 
     return (
         <div className="swiper">
             <Swiper
-            slidesPerView={4}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-              }}
-            loop={true}
-            spaceBetween={30}
-            freeMode={true}
-            pagination={{
-            clickable: true,
-            }}
-            modules={[FreeMode, Pagination, Autoplay]}
-            className="mySwiper"
+                slidesPerView={slidesPerView}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                spaceBetween={30}
+                freeMode={true}
+                pagination={showPagination ? { clickable: true } : false}
+                modules={[FreeMode, Pagination, Autoplay]}
+                className="mySwiper"
             >
-        {articlesLoading ? (<h1>Loading...</h1>) : articlesError ? ( <h1>Error...</h1>) : (
-            articles?.data.map((article) => (
-            <SwiperSlide key={article.id}>
-                
-                <p className='author-carousel'>{article.subtitle}</p>
-                <h2>{article.title}</h2>
-                <img src={article.banner} alt={article.title} /> 
-            </SwiperSlide>
-         )))} 
+                {articlesLoading ? (<h1>Loading...</h1>) : articlesError ? ( <h1>Error...</h1>) : (
+                    articles?.data.map((article) => (
+                    <SwiperSlide key={article.id}>
+                        
+                        <p className='author-carousel'>{article.subtitle}</p>
+                        <h2>{article.title}</h2>
+                        <img src={article.banner} alt={article.title} /> 
+                    </SwiperSlide>
+                )))} 
             </Swiper>
         </div>
 )
