fee576f73ecbc65dee49b4a1774c71e7d58ecbee
Mickaël <yellownanaki@gmail.com>
2023-11-02 15:49:30 +0100

    removing useless cypress config options

--- cypress.config.js
+++ cypress.config.js
@@ -5,18 +5,10 @@ module.exports = defineConfig({
     supportFile: false,
     baseUrl: "http://localhost:3000",
   },
-  integration: {
-    supportFile: false,
-    baseUrl: "http://localhost:3000",
-  },
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
-  },
-
-  env: {
-    API_URL: "https://kin-onotes-back.rover.ingeeniex.com/api/",
   },
 });
