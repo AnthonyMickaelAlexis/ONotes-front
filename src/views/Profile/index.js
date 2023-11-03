8cf8bae44cccc086aefd6217cd02cc424a7b1daf
hector colard <h1.colard@gmail.com>
2023-11-03 13:16:57 +0100

    feat/HOTFIXES: retouche style page profil

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -59,7 +59,7 @@ function ProfileView() {
             </article>
         </section>
 
-        {data &&    ( 
+        {data && ( 
             <section className='profile-view--profile_card'>
                 <img src={data.data[0].avatar ? data.data[0].avatar : 'https://picsum.photos/200/300'} alt='profile' />
                 <h3>{data.data[0].pseudo}</h3>
